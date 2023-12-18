5fc94c08c6a673f4e1e2a0b7e7409eb1ee277b3f
Crowdbotics <team@crowdbotics.com>
2023-12-18 00:39:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "taylors-home-45068",
+  url: "https://taylors-home-45068.botics.co",
+  api: "https://taylors-home-45068.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
